1338574c15a8cf720e35ad1b0147374dafa676c2
Koen <koen.node@gmail.com>
2023-08-30 21:57:35 +0800

    feat: change transition

--- components/SectionList.vue
+++ components/SectionList.vue
@@ -1,28 +1,28 @@
 <template>
-  <div class="section-list relative">
-    <div ref="container">
-      <div v-for="(item, index) in props.sections" :key="item.id"
-        class="section-box relative overflow-hidden">
-        <div class="section-head relative z-50">
-          <div class="flex-grow flex items-center">
-            <div class="drag-handler">
-              <Icon name="mdi:drag-horizontal" />
+  <div>
+    <div class="section-list" ref="container" v-loading="loading" >
+      <TransitionGroup name="list">
+        <div v-for="(item, index) in props.sections" :key="item.id" class="section-box relative overflow-hidden">
+          <div class="section-head relative z-50">
+            <div class="flex-grow flex items-center">
+              <div class="drag-handler">
+                <Icon name="mdi:drag-horizontal" />
+              </div>
+              <div class="section-order">Section {{ index + 1 }} of {{ props.sections.length }}</div>
             </div>
-            <div class="section-order">Section {{ index + 1 }} of {{ props.sections.length }}</div>
+
+            <div>
+              <el-tooltip :content="$t('Collapse Section')" placement="top" effect="light">
+                <span @click="() => $emit('collapse', item.id)">
+                  <Icon name="mdi:unfold-less-horizontal" class="cursor-pointer" />
+                </span>
+              </el-tooltip>
+              <el-tooltip :content="$t('Delete Section')" placement="top" effect="light">
+                <Icon name="mdi:trash-can-outline" class="ml-3 cursor-pointer" />
+              </el-tooltip>
+            </div>
           </div>
 
-          <div>
-            <el-tooltip :content="$t('Collapse Section')" placement="top" effect="light"  >
-              <span @click="() => $emit('collapse', item.id)">
-                <Icon name="mdi:unfold-less-horizontal" class="cursor-pointer" />
-              </span>
-            </el-tooltip>
-            <el-tooltip :content="$t('Delete Section')" placement="top" effect="light" >
-              <Icon name="mdi:trash-can-outline" class="ml-3 cursor-pointer" />
-            </el-tooltip>
-          </div>
-        </div>
-        <Transition name="section" type="animation">
           <div class="section-content" v-show="item.collapse !== true">
             <div>
               <el-input v-model="item.name" class="border-input" />
@@ -33,8 +33,8 @@
               <ElementList :sectionId="item.id" />
             </div>
           </div>
-        </Transition>
-      </div>
+        </div>
+      </TransitionGroup>
     </div>
 
     <div class="mt-9">
@@ -55,12 +55,18 @@ const props = defineProps({
   templateId: String,
   sections: {
     type: Array,
-    default: []
+    default: null
   }
 })
 
 const sectionsStore = useSectionsStore()
 const container = ref(null)
+const loading = ref(true)
+
+
+watch(props.sections, () => {
+  loading.value = false
+})
 
 useSortable(container, props.sections, {
   animation: 150,
@@ -80,58 +86,27 @@ const addSection = () => {
     template_id: props.templateId
   })
 }
-
 </script>
 
 <style lang="scss">
-@keyframes slideOutUp {
-  0% {
-    transform: translate3d(0, 0, 0);
-    position: relative;
-    opacity: 1;
-  }
-
-  40% {
-    transform: translate3d(0, -40%, 0);
-    opacity: 0.8;
-  }
-
-  80% {
-    transform: translate3d(0, -80%, 0);
-
-    opacity: 0.4;
-  }
-
-  100% {
-    opacity: 0;
-    position: absolute;
-    transform: translate3d(0, -100%, 0);
-  }
+.list-move,
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
 }
 
-@keyframes slideInDown {
-  0% {
-    visibility: hidden;
-    transform: translate3d(0, -100%, 0);
-  }
-
-  100% {
-    transform: translate3d(0, 0, 0);
-  }
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateY(-100%);
 }
 
-.section-enter-active {
-  animation-fill-mode: forwards;
-  animation: 0.5s linear slideInDown;
-}
-
-.section-leave-active {
-  animation-fill-mode: forwards;
-  animation: 0.5s linear slideOutUp;
+.list-leave-active {
+  position: absolute;
 }
 
 .section-list {
-  transition: all ease 1s;
+  min-height: 100px;
 }
 
 .section-box:nth-child(n + 2) {
