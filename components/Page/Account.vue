d265d38785aa32433342b81572e78c6738222b13
Koen <koen.node@gmail.com>
2023-10-18 14:35:36 +0800

    feat: add github link

--- components/Page/Account.vue
+++ components/Page/Account.vue
@@ -7,7 +7,14 @@
 
     <template #dropdown>
       <el-dropdown-menu>
-        <el-dropdown-item command="logout">{{ $t('Logout') }}</el-dropdown-item>
+        <el-dropdown-item command="code" >
+           <Icon name="mdi:github" size="16px" />
+           <span class="ml-2">{{ $t('Code') }}</span>
+        </el-dropdown-item>
+        <el-dropdown-item command="logout" :icon="Plus">
+          <Icon  name="mdi:logout-variant" size="16px" />
+           <span class="ml-2">{{ $t('Logout') }}</span>
+        </el-dropdown-item>
       </el-dropdown-menu>
     </template>
   </el-dropdown>
@@ -28,6 +35,9 @@ const handleAccountCommand = (command: string) => {
       logout()
       break;
 
+    case 'code':
+      window.open('https://github.com/koen-chen/HIBOX')
+      break;
     default:
       break;
   }
